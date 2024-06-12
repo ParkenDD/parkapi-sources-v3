ae173ce3f246ffb4438140d5d541b9821f94dc16
Abdullahi Fatola <fatolaabdullahi@outlook.com>
2024-06-12 12:54:38 +0200

    Updated the validator and mapper

--- src/parkapi_sources/converters/apcoa/validators.py
+++ src/parkapi_sources/converters/apcoa/validators.py
@@ -3,6 +3,7 @@ Copyright 2024 binary butterfly GmbH
 Use of this source code is governed by an MIT-style license that can be found in the LICENSE.txt.
 """
 
+from collections import defaultdict
 from datetime import datetime, timezone
 from decimal import Decimal
 from enum import Enum
@@ -19,6 +20,7 @@ from validataclass.validators import (
     Noneable,
     NumericValidator,
     StringValidator,
+    UrlValidator,
 )
 
 from parkapi_sources.models.enums import ParkingSiteType
@@ -84,11 +86,6 @@ class ApcoaOpeningHoursWeekdays(Enum):
         }.get(self, None)
 
 
-class ApcoaOpeningHoursTime:
-    OPEN_24H = '00:00 - 00:00'
-    CLOSED = 'closed'
-
-
 @validataclass
 class ApcoaCarparkTypeNameInput:
     Name: ApcoaCarparkType = EnumValidator(ApcoaCarparkType)
@@ -128,10 +125,23 @@ class ApcoaOpeningHoursInput:
 
 @validataclass
 class ApcoaCarparkPhotoURLInput:
-    CarparkPhotoURL1: Optional[str] = Noneable(StringValidator())
-    CarparkPhotoURL2: Optional[str] = Noneable(StringValidator())
-    CarparkPhotoURL3: Optional[str] = Noneable(StringValidator())
-    CarparkPhotoURL4: Optional[str] = Noneable(StringValidator())
+    CarparkPhotoURL1: Optional[str] = Noneable(UrlValidator())
+    CarparkPhotoURL2: Optional[str] = Noneable(UrlValidator())
+    CarparkPhotoURL3: Optional[str] = Noneable(UrlValidator())
+    CarparkPhotoURL4: Optional[str] = Noneable(UrlValidator())
+
+
+@validataclass
+class ApcoaIndicativeTariffInput:
+    MinPrefix: Optional[str] = Noneable(StringValidator())
+    MinValue: Optional[Decimal] = Noneable(NumericValidator())
+    MinSuffix: Optional[str] = Noneable(StringValidator())
+    MaxPrefix: Optional[str] = Noneable(StringValidator())
+    MaxValue: Optional[Decimal] = Noneable(NumericValidator())
+    MaxSuffix: Optional[str] = Noneable(StringValidator())
+    Currency: Optional[str] = Noneable(StringValidator())
+    CurrencyCode: Optional[str] = Noneable(StringValidator())
+    TaxRate: Optional[Decimal] = Noneable(NumericValidator())
 
 
 @validataclass
@@ -139,7 +149,7 @@ class ApcoaParkingSiteInput:
     CarParkId: int = IntegerValidator(allow_strings=True)
     CarparkLongName: Optional[str] = Noneable(StringValidator())
     CarparkShortName: Optional[str] = Noneable(StringValidator())
-    CarParkWebsiteURL: Optional[str] = Noneable(StringValidator())
+    CarParkWebsiteURL: Optional[str] = Noneable(UrlValidator())
     CarParkPhotoURLs: Optional[ApcoaCarparkPhotoURLInput] = Noneable(DataclassValidator(ApcoaCarparkPhotoURLInput))
     CarparkType: ApcoaCarparkTypeNameInput = DataclassValidator(ApcoaCarparkTypeNameInput)
     Address: ApcoaAdressInput = DataclassValidator(ApcoaAdressInput)
@@ -150,6 +160,7 @@ class ApcoaParkingSiteInput:
         local_timezone=ZoneInfo('Europe/Berlin'),
         target_timezone=timezone.utc,
     )
+    IndicativeTariff: Optional[ApcoaIndicativeTariffInput] = Noneable(DataclassValidator(ApcoaIndicativeTariffInput))
 
     # TODO: ignored multiple attributes which do not matter so far
 
@@ -159,3 +170,25 @@ class ApcoaParkingSiteInput:
                 return
         # If no capacity with type PARKING was found, we miss the capacity and therefore throw a validation error
         raise ValidationError(reason='Missing parking spaces capacity')
+
+    def get_osm_opening_hours(self) -> str:
+        apcoa_opening_times: dict = defaultdict(list)
+        for opening_hours_input in self.OpeningHours:
+            opening_time = opening_hours_input.OpeningTimes.replace(' ', '')
+            if opening_hours_input.OpeningTimes == '00:00 - 00:00':
+                apcoa_opening_times['24/7'].append(opening_time)
+            if opening_hours_input.Weekday in list(ApcoaOpeningHoursWeekdays)[:5]:
+                apcoa_opening_times['Mo-Fr'].append(opening_time)
+            elif opening_hours_input.Weekday in list(ApcoaOpeningHoursWeekdays):
+                apcoa_opening_times[opening_hours_input.Weekday.to_osm_opening_day_format()].append(opening_time)
+
+        osm_opening_hours = ';'.join(
+            [
+                f'{osm_opening_day} {",".join(set(opening_hours))}'
+                for osm_opening_day, opening_hours in apcoa_opening_times.items()
+                if osm_opening_day != '24/7' and 'closed' not in opening_hours
+            ]
+        )
+        if len(apcoa_opening_times['24/7']) == 7:
+            osm_opening_hours = '24/7'
+        return osm_opening_hours
