fda08fae353b4814821e48a678b97acfc1c17e3b
Abdullahi Fatola <fatolaabdullahi@outlook.com>
2024-09-09 19:04:19 +0200

    Added openinh_gours as 24/7 for open eed entry

--- src/parkapi_sources/converters/vrs_p_r/vrs_p_r.py
+++ src/parkapi_sources/converters/vrs_p_r/vrs_p_r.py
@@ -8,9 +8,13 @@ from typing import Any
 
 import pyproj
 from openpyxl.cell import Cell
+from openpyxl.workbook.workbook import Workbook
+from validataclass.exceptions import ValidationError
 
 from parkapi_sources.converters.base_converter.push import NormalizedXlsxConverter
-from parkapi_sources.models import SourceInfo
+from parkapi_sources.exceptions import ImportParkingSiteException
+from parkapi_sources.models import SourceInfo, StaticParkingSiteInput
+from parkapi_sources.models.enums import ParkAndRideType, PurposeType
 
 
 class VrsParkAndRidePushConverter(NormalizedXlsxConverter):
@@ -41,8 +45,42 @@ class VrsParkAndRidePushConverter(NormalizedXlsxConverter):
         # Other opening times are there, but not parsable
     }
 
+    def handle_xlsx(self, workbook: Workbook) -> tuple[list[StaticParkingSiteInput], list[ImportParkingSiteException]]:
+        worksheet = workbook.active
+        mapping: dict[str, int] = self.get_mapping_by_header(next(worksheet.rows))
+        row_values = [cell.value for cell in next(worksheet.rows)]
+
+        static_parking_site_errors: list[ImportParkingSiteException] = []
+        static_parking_site_inputs: list[StaticParkingSiteInput] = []
+
+        for row in worksheet.iter_rows(min_row=2):
+            # ignore empty lines as LibreOffice sometimes adds empty rows at the end of a file
+            if row[0].value is None:
+                continue
+            parking_site_dict = self.map_row_to_parking_site_dict(mapping, row)
+
+            if 'Zufahrt' in row_values:
+                if row[row_values.index('Zufahrt')].value == 'offen' or (
+                    row[row_values.index('Zufahrt')].value == 'beschrankt' and parking_site_dict['opening_hours_is_24_7'] == 'ja'
+                ):
+                    parking_site_dict['opening_hours'] = '24/7'
+
+            try:
+                static_parking_site_inputs.append(self.static_parking_site_validator.validate(parking_site_dict))
+            except ValidationError as e:
+                static_parking_site_errors.append(
+                    ImportParkingSiteException(
+                        source_uid=self.source_info.uid,
+                        parking_site_uid=parking_site_dict.get('uid'),
+                        message=f'invalid static parking site data {parking_site_dict}: {e.to_dict()}',
+                    )
+                )
+                continue
+
+        return static_parking_site_inputs, static_parking_site_errors
+
     def map_row_to_parking_site_dict(self, mapping: dict[str, int], row: list[Cell]) -> dict[str, Any]:
-        parking_site_dict: dict[str, str] = {}
+        parking_site_dict = super().map_row_to_parking_site_dict(mapping, row)
         for field in mapping.keys():
             parking_site_dict[field] = row[mapping[field]].value
 
@@ -50,6 +88,9 @@ class VrsParkAndRidePushConverter(NormalizedXlsxConverter):
         parking_site_dict['lat'] = coordinates[1]
         parking_site_dict['lon'] = coordinates[0]
 
+        parking_site_dict['opening_hours'] = parking_site_dict['opening_hours'].replace('-00:00', '-24:00')
+        parking_site_dict['purpose'] = PurposeType.CAR.name
+        parking_site_dict['park_and_ride_type'] = [ParkAndRideType.TRAIN.name]
         parking_site_dict['type'] = self.type_mapping.get(parking_site_dict.get('type'))
         parking_site_dict['static_data_updated_at'] = datetime.now(tz=timezone.utc).isoformat()
 
