7ce46da45e128b50a8930959e7e6675e96788783
Judith <judith.vockenroth@binary-butterfly.de>
2024-10-08 16:25:19 +0200

    #138 - Update data for static and realtime parking sites

--- src/parkapi_sources/converters/kienzler/models.py
+++ src/parkapi_sources/converters/kienzler/models.py
@@ -7,6 +7,7 @@ from datetime import datetime, timezone
 from decimal import Decimal
 
 from validataclass.dataclasses import validataclass
+from validataclass.helpers import UnsetValue
 from validataclass.validators import IntegerValidator, NumericValidator, StringValidator
 
 from parkapi_sources.models import RealtimeParkingSiteInput, StaticParkingSiteInput
@@ -34,6 +35,8 @@ class KienzlerInput:
             type=ParkingSiteType.LOCKERS,
             static_data_updated_at=datetime.now(tz=timezone.utc),
             public_url=f'{base_url}/order/booking/?preselect_unit_uid={self.id[4:]}',
+            opening_hours='24/7',
+            has_fee=True,
         )
 
     def to_realtime_parking_site(self) -> RealtimeParkingSiteInput:
@@ -42,4 +45,5 @@ class KienzlerInput:
             realtime_data_updated_at=datetime.now(tz=timezone.utc),
             realtime_capacity=self.sum_boxes,
             realtime_free_capacity=self.bookable,
+            realtime_opening_status=UnsetValue,
         )
