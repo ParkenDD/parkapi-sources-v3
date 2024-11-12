ad2641a0d763e439cbde96cc04db83bb4717de82
Judith <judith.vockenroth@binary-butterfly.de>
2024-11-12 17:34:25 +0100

    #161 - Add test for use GeoJSON data as additional data input

--- tests/converters/kienzler_test.py
+++ tests/converters/kienzler_test.py
@@ -25,11 +25,26 @@ def requests_mock_kienzler(requests_mock: Mocker) -> Mocker:
 
 
 @pytest.fixture
+def requests_mock_kienzler_with_geojson(requests_mock: Mocker) -> Mocker:
+    json_path = Path(Path(__file__).parent, 'data', 'kienzler.geojson')
+    with json_path.open() as json_file:
+        json_data = json_file.read()
+
+    requests_mock.get(
+        'https://raw.githubusercontent.com/ParkenDD/parkapi-static-data/refs/heads/main/sources/kienzler_bike_and_ride.geojson',
+        text=json_data,
+    )
+
+    return requests_mock
+
+
+@pytest.fixture
 def kienzler_config_helper(mocked_config_helper: Mock):
     config = {
         'PARK_API_KIENZLER_BIKE_AND_RIDE_USER': '01275925-742c-460b-8778-eca90eb114bc',
         'PARK_API_KIENZLER_BIKE_AND_RIDE_PASSWORD': '626027f2-66e9-40bd-8ff2-4c010f5eca05',
         'PARK_API_KIENZLER_BIKE_AND_RIDE_IDS': 'id1,id2,id3',
+        'STATIC_GEOJSON_BASE_URL': 'https://raw.githubusercontent.com/ParkenDD/parkapi-static-data/refs/heads/main/sources',
     }
     mocked_config_helper.get.side_effect = lambda key, default=None: config.get(key, default)
     return mocked_config_helper
@@ -40,6 +55,11 @@ def kienzler_pull_converter(kienzler_config_helper: Mock) -> KienzlerBikeAndRide
     return KienzlerBikeAndRidePullConverter(config_helper=kienzler_config_helper)
 
 
+@pytest.fixture
+def kienzler_pull_converter_with_geojson(kienzler_config_helper: Mock) -> KienzlerBikeAndRidePullConverter:
+    return KienzlerBikeAndRidePullConverter(config_helper=kienzler_config_helper, use_geojson=True)
+
+
 class KienzlerPullConverterTest:
     @staticmethod
     def test_get_static_parking_sites(
@@ -47,6 +67,21 @@ class KienzlerPullConverterTest:
         requests_mock_kienzler: Mocker,
     ):
         static_parking_site_inputs, import_parking_site_exceptions = kienzler_pull_converter.get_static_parking_sites()
+
+        assert len(static_parking_site_inputs) == 5
+        assert len(import_parking_site_exceptions) == 0
+
+        validate_static_parking_site_inputs(static_parking_site_inputs)
+
+    @staticmethod
+    def test_get_static_parking_sites_with_geojson(
+        kienzler_pull_converter_with_geojson: KienzlerBikeAndRidePullConverter,
+        requests_mock_kienzler: Mocker,
+        requests_mock_kienzler_with_geojson: Mocker,
+    ):
+        static_parking_site_inputs, import_parking_site_exceptions = (
+            kienzler_pull_converter_with_geojson.get_static_parking_sites()
+        )
 
         assert len(static_parking_site_inputs) == 5
         assert len(import_parking_site_exceptions) == 0
