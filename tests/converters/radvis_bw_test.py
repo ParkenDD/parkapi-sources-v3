34d36fdc61bdce7df910087f4cf150d169f57bb9
Abdullahi Fatola <fatolaabdullahi@outlook.com>
2024-11-18 11:45:32 +0100

    Added Enum Bildungseinrichtung for abstellanlagen_ort

--- tests/converters/radvis_bw_test.py
+++ tests/converters/radvis_bw_test.py
@@ -42,7 +42,7 @@ class RadvisBwConverterTest:
         )
         static_parking_site_inputs, import_parking_site_exceptions = radvis_bw_pull_converter.get_static_parking_sites()
 
-        assert len(static_parking_site_inputs) == 761
-        assert len(import_parking_site_exceptions) == 0
+        assert len(static_parking_site_inputs) == 690
+        assert len(import_parking_site_exceptions) == 1
 
         validate_static_parking_site_inputs(static_parking_site_inputs)
