7d961a61f52d35a32e57031ca975bc7878827e79
Judith <judith.vockenroth@binary-butterfly.de>
2026-06-05 11:47:59 +0200

    [PARK-5] Add header and delete duplicated enum classes

--- src/parkapi_sources/converters/friedrichshafen_easypark/models.py
+++ src/parkapi_sources/converters/friedrichshafen_easypark/models.py
@@ -5,7 +5,6 @@ Use of this source code is governed by an MIT-style license that can be found in
 
 from datetime import datetime, timezone
 from decimal import Decimal
-from enum import Enum
 
 import shapely
 from shapely import GeometryType, LineString
@@ -13,37 +12,18 @@ from validataclass.dataclasses import validataclass
 from validataclass.validators import EnumValidator, IntegerValidator, StringValidator
 
 from parkapi_sources.models import PurposeType, StaticParkingSiteInput
-from parkapi_sources.models.enums import ParkingSiteOrientation, ParkingSiteType
+from parkapi_sources.models.enums import ParkingSiteOrientation, ParkingSiteSide, ParkingSiteType
 from parkapi_sources.util import round_7d
 from parkapi_sources.validators import ExcelNoneable, GermanDecimalValidator
 from parkapi_sources.validators.csv_geojson_geometry_validator import CsvGeoJSONGeometryValidator
-
-
-class ParkAngle(Enum):
-    PARALLEL = 'parallel'
-    PERPENDICULAR = 'perpendicular'
-    DIAGONAL = 'diagonal'
-
-    def to_parkind_site_orientation(self) -> ParkingSiteOrientation:
-        if self == ParkingSiteOrientation.PARALLEL:
-            return ParkingSiteOrientation.PARALLEL
-        if self == ParkingSiteOrientation.PERPENDICULAR:
-            return ParkingSiteOrientation.PERPENDICULAR
-        if self == ParkingSiteOrientation.DIAGONAL:
-            return ParkingSiteOrientation.DIAGONAL
-
-
-class StreetSide(Enum):
-    RIGHT = 'right'
-    LEFT = 'left'
 
 
 @validataclass
 class FriedrichshafenEasyParkRowInput:
     id: int = IntegerValidator(allow_strings=True)
     length: Decimal = GermanDecimalValidator()
-    park_angle: ParkAngle = EnumValidator(ParkAngle)
-    street_side: StreetSide = EnumValidator(StreetSide)
+    park_angle: ParkingSiteOrientation = EnumValidator(ParkingSiteOrientation)
+    street_side: ParkingSiteSide = EnumValidator(ParkingSiteSide)
     location_on_sidewalk: LineString = CsvGeoJSONGeometryValidator(allowed_geometry_types=[GeometryType.LINESTRING])
     permissions_translation: str = StringValidator()
     permission_period: str | None = ExcelNoneable(StringValidator())
@@ -56,7 +36,7 @@ class FriedrichshafenEasyParkRowInput:
             uid=str(self.id),
             type=ParkingSiteType.ON_STREET,
             capacity=self._get_capacity(),
-            orientation=self.park_angle.to_parkind_site_orientation(),
+            orientation=self.park_angle,
             name='Straßenparkplatz',
             purpose=PurposeType.CAR,
             side=self.street_side,
@@ -79,9 +59,9 @@ class FriedrichshafenEasyParkRowInput:
         return fee_description
 
     def _get_capacity(self) -> int:
-        if self.park_angle == ParkAngle.PARALLEL:
+        if self.park_angle == ParkingSiteOrientation.PARALLEL:
             return int(self.length / Decimal('5'))
-        if self.park_angle == ParkAngle.PERPENDICULAR:
+        if self.park_angle == ParkingSiteOrientation.PERPENDICULAR:
             return int(self.length / Decimal('2.5'))
-        if self.park_angle == ParkAngle.DIAGONAL:
+        if self.park_angle == ParkingSiteOrientation.DIAGONAL:
             return int(self.length / Decimal('3'))
